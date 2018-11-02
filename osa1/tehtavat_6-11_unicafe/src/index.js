2b82ed0cf8a0191d22107d84a614c92ce1dbbf61
Virva <virva.svala@gmail.com>
2018-11-02 17:48:53 +0200

    osa1 tehtava_1.6 Unicafe osa1

--- osa1/tehtavat_6-11_unicafe/src/index.js
+++ osa1/tehtavat_6-11_unicafe/src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hyva: 0,
+      neutraali:0,
+      huono:0
+    }
+  }
+  klikHyva = () => {
+  this.setState({
+    hyva: this.state.hyva + 1
+  })
+}
+klikNeutraali = () => {
+  this.setState({
+    neutraali: this.state.neutraali + 1
+  })
+}
+klikHuono = () => {
+  this.setState({
+    huono: this.state.huono + 1
+  })
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render(){
+
+    const teksti = {
+      otsikko1: 'Anna Palautetta',
+      otsikko2: 'Statistiikka',
+      stat1: 'hyvä',
+      stat2: 'neutraali',
+      stat3: 'huono'
+    }
+
+    return (
+      <div>
+
+         <h1>{teksti.otsikko1}</h1>
+         <div>
+              <button onClick={this.klikHyva}>hyva</button>
+               <button onClick={this.klikNeutraali}>neutraali</button>
+               <button onClick={this.klikHuono}>huono</button>
+             </div>
+
+
+         <h1>{teksti.otsikko2}</h1>
+         <div>
+         <p>{teksti.stat1} {this.state.hyva}</p>
+         <p>{teksti.stat2} {this.state.neutraali}</p>
+         <p>{teksti.stat3} {this.state.huono}</p>
+            </div>
+     </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
