c709a0b4139e49eceadf096d35e99df8438fa91c
Virva <virva.svala@gmail.com>
2018-11-16 21:00:42 +0200

    osa2 tehtava_2.4 monta kurssia

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -1,53 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Kurssi=({kurssi})=> {
-  const rivit = () => kurssi.osat.map(osa =>
-  <p key={osa.id}>
-  {osa.nimi}, {osa.tehtavia}</p>)
-  return(
-  <p>{rivit()}</p>
+
+const Kurssi = ({kurssi}) => {
+  return (
+      <div>
+          <Otsikko nimi={kurssi.nimi} />
+          <Sisalto kurssi={kurssi} />
+          <Yhteensa kurssi={kurssi} />
+      </div>
+  )
+}
+const Otsikko = ({nimi}) => {
+  return (
+      <div>
+          <h1>{nimi}</h1>
+      </div>
   )
 }
 
-const App = () => { 
-  const tehtavat = () => kurssi.osat.map(osa =>osa.tehtavia)
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10,
-        id: 1
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7,
-          id: 2
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14,
-         id: 3
-      },
-      {
-      nimi: 'redux',
-      tehtavia: 7,
-      id: 4
-    }
-    ]
-  }
-
-    return (
+const Sisalto = ({kurssi}) => {
+  return (
       <div>
-       
-      <h1>{kurssi.nimi}</h1>
-      <Kurssi kurssi = {kurssi}/>
-      <p>yhteensa  {tehtavat().reduce(reducer)} tehtavaa</p> 
+          {kurssi.osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>)}
       </div>
-      )
-    }
+  )
+}
+
+const Yhteensa = ({kurssi}) => {
+  const tehtavat = kurssi.osat.map(osa => osa.tehtavia)
+  const reducer = (accumulator, currentValue) => (accumulator + currentValue);
+  return (
+      <div>
+          <p>yhteensä {tehtavat.reduce(reducer, 0)} tehtävää</p>
+      </div>
+  )
+
+}
+
+
+const App = () => {
+   const kurssit = [
+     {
+       nimi: 'Half Stack -sovelluskehitys',
+       id: 1,
+       osat: [
+         {
+           nimi: 'Reactin perusteet',
+           tehtavia: 10,
+           id: 1
+         },
+         {
+           nimi: 'Tiedonvälitys propseilla',
+           tehtavia: 7,
+           id: 2
+         },
+         {
+           nimi: 'Komponenttien tila',
+           tehtavia: 14,
+           id: 3
+         }
+       ]
+     },
+     {
+       nimi: 'Node.js',
+       id: 2,
+       osat: [
+         {
+           nimi: 'Routing',
+           tehtavia: 3,
+           id: 1
+         },
+         {
+           nimi: 'Middlewaret',
+           tehtavia: 7,
+           id: 2
+         }
+       ]
+     }
+   ]
+
+
+  return (
+    <div>
+    <h1>Opetusohjelma</h1>
+
+     <ul>
+    {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
+    </ul>
+    </div>
+    )
+  }
 
 ReactDOM.render(
   <App />,
