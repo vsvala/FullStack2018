67412534f058162d1d592e5ba367d1632e036106
Virva <virva.svala@gmail.com>
2018-11-16 19:52:43 +0200

    osa2 tehtava_2.2 tehtavien määrä

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -4,11 +4,9 @@ import ReactDOM from 'react-dom';
 const Kurssi=({kurssi})=> {
   const rivit = () => kurssi.osat.map(osa =>
   <p key={osa.id}>
-   {osa.nimi}, {osa.tehtavia}</p>)
+  {osa.nimi}, {osa.tehtavia}</p>)
   return(
-
   <p>{rivit()}</p>
-
   )
 }
 
@@ -30,15 +28,21 @@ const App = () => {
         nimi: 'Komponenttien tila',
         tehtavia: 14,
          id: 3
-      }
-
+      },
+      {
+      nimi: 'redux',
+      tehtavia: 7,
+      id: 4
+    }
     ]
   }
 
     return (
       <div>
+       
       <h1>{kurssi.nimi}</h1>
       <Kurssi kurssi = {kurssi}/>
+      <p>yhteensa {kurssi.osat[0].tehtavia+kurssi.osat[1].tehtavia+kurssi.osat[2].tehtavia+kurssi.osat[3].tehtavia} tehtavaa</p> 
       </div>
       )
     }
