27a8a0e2582c84efbd23dbc5df0b244364a0c8fa
Virva <virva.svala@gmail.com>
2018-11-16 21:51:43 +0200

    osa2 tehtava_2.5 erilllinen moduuli

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -1,43 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-const Kurssi = ({kurssi}) => {
-  return (
-      <div>
-          <Otsikko nimi={kurssi.nimi} />
-          <Sisalto kurssi={kurssi} />
-          <Yhteensa kurssi={kurssi} />
-      </div>
-  )
-}
-const Otsikko = ({nimi}) => {
-  return (
-      <div>
-          <h1>{nimi}</h1>
-      </div>
-  )
-}
-
-const Sisalto = ({kurssi}) => {
-  return (
-      <div>
-          {kurssi.osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>)}
-      </div>
-  )
-}
-
-const Yhteensa = ({kurssi}) => {
-  const tehtavat = kurssi.osat.map(osa => osa.tehtavia)
-  const reducer = (accumulator, currentValue) => (accumulator + currentValue);
-  return (
-      <div>
-          <p>yhteensä {tehtavat.reduce(reducer, 0)} tehtävää</p>
-      </div>
-  )
-
-}
-
+import Kurssi from './components/Kurssi'
 
 const App = () => {
    const kurssit = [
