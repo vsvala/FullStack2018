b6496af785a03df55d039703a3e3bbf7e11ab8a8
Virva <virva.svala@gmail.com>
2018-12-02 23:16:01 +0200

    Osa5 tehtava 5.17 ja 5.18 Unicfe revisited1-2

--- Redux-Unicafe_osa5/src/reducer.test.js
+++ Redux-Unicafe_osa5/src/reducer.test.js
@@ -1,22 +1,78 @@
-import noteReducer from './noteReducer'
+import counterReducer from './reducer'
 import deepFreeze from 'deep-freeze'
 
-describe('noteReducer', () => {
-    it('returns new state with action NEW_NOTE', () => {
-      const state = []
-      const action = {
-        type: 'NEW_NOTE',
-        data: {
-          content: 'sovelluksen tila talletetaan storeen',
-          important: true,
-          id: 1
-        }
-      }
-  
-      deepFreeze(state)
-      const newState = noteReducer(state, action)
-  
-      expect(newState.length).toBe(1)
-      expect(newState).toContainEqual(action.data)
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  it('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+
+
+  it('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
     })
   })
+
+  it('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  it('ok is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+  it('ok is incremented', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+    
+  })
+})
